2bd15200474606950ed17c22801517e419a7ba8b
Denise Chang <ddleletan@hotmail.com>
2024-11-25 09:58:33 -0500

    clean repository

--- scripts/02-download_data.R
+++ scripts/02-download_data.R
@@ -1,25 +1,25 @@
 #### Preamble ####
-# Purpose: Downloads and saves the data from [...UPDATE THIS...]
-# Author: Rohan Alexander [...UPDATE THIS...]
-# Date: 11 February 2023 [...UPDATE THIS...]
-# Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
+# Purpose: Downloads and saves the data from Open Data Toronto
+# Author: Denise Chang
+# Date: 20 November 2024
+# Contact: dede.chang@utoronto.ca
 # License: MIT
-# Pre-requisites: [...UPDATE THIS...]
-# Any other information needed? [...UPDATE THIS...]
+# Pre-requisites: None
 
 
 #### Workspace setup ####
 library(opendatatoronto)
 library(tidyverse)
-# [...UPDATE THIS...]
+library(arrow)
 
 #### Download data ####
 # [...ADD CODE HERE TO DOWNLOAD...]
 
+# Code to download
+
 
 
 #### Save data ####
-# [...UPDATE THIS...]
 # change the_raw_data to whatever name you assigned when you downloaded it.
 write_csv(the_raw_data, "inputs/data/raw_data.csv") 
 
