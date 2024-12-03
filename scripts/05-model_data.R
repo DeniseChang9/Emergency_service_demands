77699542c1aae2f544dd507793b09fabf237cf34
Denise Chang <ddleletan@hotmail.com>
2024-12-03 06:06:40 -0500

    builds model in script

--- scripts/05-model_data.R
+++ scripts/05-model_data.R
@@ -1,37 +1,45 @@
 #### Preamble ####
-# Purpose: Models... [...UPDATE THIS...]
-# Author: Rohan Alexander [...UPDATE THIS...]
-# Date: 11 February 2023 [...UPDATE THIS...]
-# Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
+# Purpose: Models number of calls based on temporal factors, incident type and average units on incident scene
+# Author: Denise Chang
+# Date: 2 December 2024
+# Contact: dede.chang@mail.utoronto.ca
 # License: MIT
-# Pre-requisites: [...UPDATE THIS...]
-# Any other information needed? [...UPDATE THIS...]
-
+# Pre-requisites: 02-download_data.R and 03-clean_data.R
 
 #### Workspace setup ####
 library(tidyverse)
 library(rstanarm)
+library(arrow)
 
-#### Read data ####
-analysis_data <- read_csv("data/analysis_data/analysis_data.csv")
+#### Model data ####
+# Read data
+clean_data <-
+  read_parquet("data/02-analysis_data/analysis_data.parquet")
 
-### Model data ####
-first_model <-
-  stan_glm(
-    formula = flying_time ~ length + width,
-    data = analysis_data,
-    family = gaussian(),
-    prior = normal(location = 0, scale = 2.5, autoscale = TRUE),
-    prior_intercept = normal(location = 0, scale = 2.5, autoscale = TRUE),
-    prior_aux = exponential(rate = 1, autoscale = TRUE),
-    seed = 853
+# Prepare data
+clean_data <- clean_data |>
+  mutate(
+    avg_units_arrived = as.numeric(avg_units_arrived),
+    month = factor(month, levels = month.abb, ordered = TRUE),
+    day_of_week = factor(
+      day_of_week,
+      levels = c("Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"),
+      ordered = TRUE
+    ),
+    incident_type = factor(incident_type)
   )
 
-
-#### Save model ####
-saveRDS(
-  first_model,
-  file = "models/first_model.rds"
+# Fit the model using rstanarm
+incident_model <- stan_glm(
+  formula = count ~ year + month + day_of_week + hour + incident_type + avg_units_arrived,
+  data = clean_data,
+  family = gaussian(),
+  prior = normal(0, 2.5),
+  prior_intercept = normal(0, 2.5),
+  prior_aux = exponential(1),
+  seed = 123
 )
 
-
+#### Save model ####
+saveRDS(incident_model,
+        file = "models/incident_model.rds")
